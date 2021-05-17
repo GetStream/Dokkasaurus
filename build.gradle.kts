2b476be9e1e821af038f219b8e7992753f5c3dad
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-05-17 11:28:40 +0200

    Use gradle script to push artifacts to mavenCentral

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,14 +9,19 @@ plugins {
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
-group = "io.getstream"
-version = "0.1.7-SNAPSHOT"
+apply(from = "${rootDir}/scripts/publish.gradle")
+
 val dokkaVersion: String by project
-val publicationName="Dokkasaurus"
 
 repositories {
     mavenCentral()
     jcenter()
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
 }
 
 dependencies {
@@ -27,124 +32,4 @@ dependencies {
     testImplementation(kotlin("test-junit"))
     testImplementation("org.jetbrains.dokka:dokka-test-api:$dokkaVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
-}
-
-val dokkaOutputDir = "$buildDir/dokka"
-
-tasks {
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-    dokkaHtml {
-        outputDirectory.set(file(dokkaOutputDir))
-    }
-}
-
-val javadocJar by tasks.registering(Jar::class) {
-    dependsOn(tasks.dokkaHtml)
-    archiveClassifier.set("javadoc")
-    from(dokkaOutputDir)
-}
-
-java {
-    withSourcesJar()
-}
-
-publishing {
-    publications {
-        val dokkasaurusPlugin by creating(MavenPublication::class) {
-            artifactId = project.name
-            from(components["java"])
-            artifact(javadocJar.get())
-
-            pom {
-                name.set(publicationName)
-                description.set("A plugin for Dokka to generate markdown files compatible with Docusaurus")
-                url.set("https://github.com/Kotlin/dokka-plugin-template/")
-
-                licenses {
-                    license {
-
-                        name.set("Stream License")
-                        url.set("https://github.com/GetStream/Dokkasaurus/blob/master/LICENSE")
-                        distribution.set("repo")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("leandroBorgesFerreira")
-                        name.set("Leandro Borges Ferreira")
-                        email.set("leandro@getstream.io")
-                        organization.set("Stream")
-                        organizationUrl.set("https://getstream.io/")
-                    }
-                    developer {
-                        id.set("jcminarro")
-                        name.set("Jc Miñarro")
-                        organization.set("Stream")
-                        email.set("josecarlos@getstream.io")
-                        organizationUrl.set("https://getstream.io/")
-                    }
-                    developer {
-                        id.set("adasiewiczr")
-                        name.set("Rafal Adasiewicz")
-                        organization.set("Stream")
-                        email.set("rafal@getstream.io")
-                        organizationUrl.set("https://getstream.io/")
-                    }
-                    developer {
-                        id.set("ogkuzmin")
-                        name.set("Oleg Kuzmin")
-                        organization.set("Stream")
-                        email.set("oleg@getstream.io")
-                        organizationUrl.set("https://getstream.io/")
-                    }
-                    developer {
-                        id.set("zsmb13")
-                        name.set("Márton Braun")
-                        organization.set("Stream")
-                        email.set("marton@getstream.io")
-                        organizationUrl.set("https://getstream.io/")
-                    }
-                    developer {
-                        id.set("bychkovdmitry")
-                        name.set("Dmitrii Bychkov")
-                        organization.set("Stream")
-                        email.set("dmitrii@getstream.io")
-                        organizationUrl.set("https://getstream.io/")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:github.com/getstream/dokkasaurus.git")
-                    url.set("https://github.com/getstream/dokkasaurus/tree/master")
-                }
-            }
-        }
-
-        signPublicationsIfKeyPresent(dokkasaurusPlugin)
-    }
-}
-
-fun Project.signPublicationsIfKeyPresent(publication: MavenPublication) {
-    val signingKey: String? = System.getenv("SIGN_KEY")
-    val signingKeyPassphrase: String? = System.getenv("SIGN_KEY_PASSPHRASE")
-
-    if (!signingKey.isNullOrBlank()) {
-        extensions.configure<SigningExtension>("signing") {
-            useInMemoryPgpKeys(signingKey, signingKeyPassphrase)
-            sign(publication)
-        }
-    }
-}
-
-// Set up Sonatype repository
-nexusPublishing {
-    repositories {
-        sonatype {
-            username.set(System.getenv("OSSRH_USERNAME"))
-            password.set(System.getenv("OSSRH_PASSWORD"))
-        }
-    }
 }
