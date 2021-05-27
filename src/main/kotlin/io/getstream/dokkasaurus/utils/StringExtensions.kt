2670242e662ff5ffbb2389df7d5af3cfc1cc47e0
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-05-27 10:25:45 +0200

    Create an String extensions to convert from camelCase to snake-case

--- src/main/kotlin/io/getstream/dokkasaurus/utils/StringExtensions.kt
+++ src/main/kotlin/io/getstream/dokkasaurus/utils/StringExtensions.kt
@@ -1,5 +1,12 @@
 package io.getstream.dokkasaurus.utils
+private val camelRegex = "(?<=[a-zA-Z])[A-Z]".toRegex()
 
 internal fun String.simpleScapeTags(): String {
     return this.replace("<", "&lt;").replace(">", "&gt;")
+}
+
+internal fun String.camelToSnakeCase(): String {
+    return camelRegex.replace(this) {
+        "-${it.value}"
+    }.toLowerCase()
 }
